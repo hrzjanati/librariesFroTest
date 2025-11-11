e771ec208147bfc3ea8ce6f03214507ffd9cbc7b
Nik <hrzjanati@gmail.com>
2025-11-11 18:23:41 +0330

    fix some bug in html

--- main/views/library.py
+++ main/views/library.py
@@ -8,14 +8,23 @@ def library_detail(request, id):
 
 def library_list(request, province_id=None):
     provinces = Province.objects.all()
-    if province_id:
-        libraries = Library.objects.filter(province_id=province_id)
-    else:
-        libraries = Library.objects.all()
+    province_ids = request.GET.get('provinces', '')
+
+    libraries = Library.objects.all()
+    if province_ids:
+        ids_list = [int(i) for i in province_ids.split(',') if i.isdigit()]
+        libraries = libraries.filter(province_id__in=ids_list)
+    elif province_id:
+        libraries = libraries.filter(province_id=province_id)
 
     context = {
         'provinces': provinces,
         'libraries': libraries,
-        'selected_province': province_id,
+        'selected_provinces': province_ids.split(',') if province_ids else [],
     }
+
+    # اگر AJAX هست فقط partial HTML کتابخانه‌ها برگرده
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'library/_libraries_list.html', {'libraries': libraries})
+
     return render(request, 'library/library_list.html', context)
