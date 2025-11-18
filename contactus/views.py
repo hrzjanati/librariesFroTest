3a62d1b99cca99c81744f19c15d23075648f77a7
Nik <hrzjanati@gmail.com>
2025-11-18 14:19:54 +0330

    add telegram notification

--- contactus/views.py
+++ contactus/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from .models import ContactMessage
+import requests
 
-@csrf_exempt
+BOT_TOKEN = "8178056523:AAG1roNPcFSacGrNhtpMXpiu90xAQhnXxhs"
+CHAT_ID = "82041680"
+
 def contact_us(request):
     if request.method == "POST":
         name = request.POST.get("name")
@@ -25,18 +26,19 @@ def contact_us(request):
             message=message
         )
 
-        # HTML برای toast + پاک کردن فرم و محو شدن بعد ۵ ثانیه
+        # ارسال همزمان به تلگرام
+        text = f"📩 پیام جدید از فرم ارتباط با ما:\n\nنام: {name}\nشماره: {phone}\nایمیل: {email}\nموضوع: {subject}\nپیام: {message}"
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+
+        # HTML برای toast + پاک کردن فرم
         return HttpResponse("""
-            <div id="toast" class="fixed top-4 right-4 bg-green-500 text-white px-4 py-2 rounded shadow-md">
+            <div class="fixed top-4 right-4 bg-green-500 text-white px-4 py-2 rounded shadow-md">
                 پیام شما با موفقیت ارسال شد!
             </div>
             <script>
                 const form = document.currentScript.closest('form');
-                form.reset();  // پاک کردن فیلدها
-                setTimeout(() => {
-                    const toast = document.getElementById('toast');
-                    if(toast) toast.remove();  // محو کردن toast بعد از ۵ ثانیه
-                }, 5000);
+                form.reset();
             </script>
         """)
 
